e1eb3c152d314d87c2f537ab17a0b204e9841fb6
laijintao <18811526955@163.com>
2019-06-13 17:04:06 +0800

    swiper finished

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of imgList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" />
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'HomeSwiper',
+    data () {
+        return {
+            swiperOption: {
+                pagination: '.swiper-pagination',       //通过这个显示下面轮播的小点
+                loop: true
+            },
+            imgList: [
+                {                     
+                    id: '001',
+                    imgUrl: 'https://img1.qunarzz.com/qs/1906/49/868ffd4c5f690b02.jpg'
+                },{
+                    id: '002',
+                    imgUrl: 'https://img1.qunarzz.com/qs/1901/f4/58144f72caf30a02.jpg'
+                },{
+                    id: '003',
+                    imgUrl: 'https://img1.qunarzz.com/qs/1905/92/d0a7f0687efa3f02.jpg'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active         
+    background: #fff
+.wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 54.488%
+    background: #eeeeee
+    .swiper-img
+        width: 100%
+</style>
